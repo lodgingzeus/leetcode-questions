aaed3f1b964962d3f37461baf971e7ac3d5a1c49
lodgingzeus <bhagatd585@gmail.com>
2023-01-19 20:58:41 +0530

    q

---
+++ question-1009.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin >> n;
+
+    int m = n;
+    int mask = 0;
+
+    if (n == 0)
+        return 1;
+        
+    while(m != 0){
+        mask = (mask << 1) | 1;
+        m = m >> 1;
+    }
+    int ans = (~n) & mask;
+    
+    return ans;
+
+    return 0;
+}
---
+++ question-7.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main(){
+    int x = 321;
+    int ans = 0;
+        
+        while(x != 0){
+        if( (ans > INT_MAX/10) || (ans < INT_MIN/10) ){
+            return 0;
+        }
+        int digit = x % 10;
+        
+        ans = (ans * 10) + digit;
+        x /= 10;
+    }
+    return ans;
+}
