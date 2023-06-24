fb46894f80d5efd0d20a7b1431e0479859a1eada
lodgingzeus <bhagatd585@gmail.com>
2023-06-24 23:37:19 +0530

    qq

---
+++ coding-ninja-questions/reverse-an-array.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std; 
+#include <bits/stdc++.h> 
+void reverseArray(vector<int> &arr , int m)
+{
+	int s = m + 1;
+	int e = arr.size() - 1;
+
+	while(s <= e ){
+		swap(arr[s], arr[e]);
+		s++;
+		e--;
+	}
+}
+
+int main() {
+
+    return 0;
+}
