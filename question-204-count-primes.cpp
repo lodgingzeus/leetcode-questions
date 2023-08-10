c1c83de27c988a6c64cfef4fa8c6e817c9d15bdf
lodgingzeus <bhagatd585@gmail.com>
2023-08-10 23:38:03 +0530

    q

---
+++ question-204-count-primes.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int countPrimes(int n) {
+        int ans = 0;
+        vector<bool> prime(n+1, true);
+
+        prime[0] = prime[1] = false;
+
+        for(int i = 2; i < n; i++){
+            if(prime[i]){
+                ans++;
+
+                for(int j = 2 * i; j < n; j = j + i){
+                    prime[j] = 0;
+                }
+            }
+        }
+        return ans;
+    }
+};
+int main(){
+
+
+
+    return 0;
+}
