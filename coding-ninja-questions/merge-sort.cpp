439b96df6b87197c4fc4f0d38a3db8e63e553c0e
lodgingzeus <bhagatd585@gmail.com>
2023-10-02 13:17:05 +0530

    qq

---
+++ coding-ninja-questions/merge-sort.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+void merge(vector<int> &arr, int low, int mid, int high){
+    vector<int> temp;
+
+    int left = low;
+    int right = mid + 1;
+
+    while( left <= mid && right <= high){
+        if(arr[left] <= arr[right]){
+            temp.push_back(arr[left]);
+            left++;
+        }else{
+            temp.push_back(arr[right]);
+            right++;
+        }
+    }
+
+    while(left <= mid){
+        temp.push_back(arr[left]);
+        left++;
+    }
+
+    while(right <= high){
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    for(int i = low; i <= high; i++){
+        arr[i] = temp[i - low];
+    }
+
+}
+
+void solve(vector<int> &arr, int low, int high){
+    if (low >= high) return;
+
+    int mid = (low + high)/2;
+    solve(arr, low, mid);
+    solve(arr, mid + 1, high);
+    merge(arr, low, mid, high);
+}
+
+void mergeSort(vector < int > & arr, int n) {
+    solve(arr, 0, n-1);
+}
+int main(){
+
+
+
+    return 0;
+}
